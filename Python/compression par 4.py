38589a2ee9141e0b4a48af1e1b7d063888fad9b9
N0aaz <aazdu03@gmail.com>
2017-04-23 12:29:09 +0200

    Exemple de modification

--- Python/compression par 4.py
+++ Python/compression par 4.py
@@ -11,7 +11,7 @@ print(b)
     
 for i in range(0,int(l[0]/2)):
     for j in range(0,int(l[1]/2)):
-        b[i][j]=(a[2*i][2*j]+a[2*i+1][2*j]+a[2*i][2*j+1]+a[2*i+1][2*j+1])/4
+        b[i][j]=(a[2*i][2*j] + a[2*i+1][2*j] + a[2*i][2*j+1] + a[2*i+1][2*j+1] )/4
 print(b)
 print(b.shape)
 im.imsave("testA.png",b)
